82a4dc5612982a4a7d4f37098456f4a0ee1b7e2e
Ayoubioussoufa <Ayoubioussoufa@gmail.com>
2024-09-12 23:18:51 +0100

    leaderboard almost done git status

---
+++ script.js
@@ -0,0 +1,2 @@
+document.getElementById('player-level').textContent = '30'; // Example for updating player's level
+document.getElementById('player-wins').textContent = '150'; // Example for updating player's wins
